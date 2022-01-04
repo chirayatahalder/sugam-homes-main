9b28effde6409b5d2cfecd77c418e6684456223c
Chirayata Halder <84263108+chirayatahalder@users.noreply.github.com>
2022-01-04 10:57:41 +0530

    Sugam Homes

---
+++ sugam-homes-main/js/slick-slider.js
@@ -0,0 +1,38 @@
+$("#home-banner").slick({
+  autoplay: true,
+  infinite: true,
+  speed: 500,
+  cssEase: "linear",
+  arrows: false,
+});
+$("#home-update-banner").slick({
+  autoplay: true,
+  infinite: true,
+  speed: 500,
+  cssEase: "linear",
+  arrows: false,
+});
+$(".home-update-banner").slick({
+  infinite: true,
+  autoplay: true,
+  speed: 100,
+  fade: true,
+  cssEase: "linear",
+  arrows: false,
+});
+$(".footer-slider").slick({
+  infinite: true,
+  autoplay: true,
+  speed: 300,
+  fade: true,
+  cssEase: "linear",
+  arrows: false,
+});
+$(".urban_lakes_banner").slick({
+  infinite: true,
+  autoplay: true,
+  speed: 800,
+  // fade: true,
+  cssEase: "linear",
+  arrows: false,
+});
